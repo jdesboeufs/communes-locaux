920580a9cc6bc939c1fe466fa79b3429054b8ce8
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-12-16 16:56:58 +0100

    New implementation with @etalab/majic 0.9

--- build.js
+++ build.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-const {Transform} = require('stream')
+require('dotenv').config()
+
 const {join} = require('path')
+const bluebird = require('bluebird')
 const {chain, keyBy} = require('lodash')
-const getStream = require('get-stream')
 const {outputJson} = require('fs-extra')
-const {parse} = require('@etalab/majic')
+const {getCommuneData} = require('@etalab/majic')
 const communes = require('@etalab/decoupage-administratif/data/communes.json')
   .filter(c => ['arrondissement-municipal', 'commune-actuelle'].includes(c.type))
 
@@ -13,85 +14,57 @@ const communesIndex = keyBy(communes, 'code')
 const ACCEPTED_CATEGORIES_LOCAUX = [
   'maison',
   'appartement',
-  'commerce-sans-boutique',
-  'divers',
-  'commerce-boutique',
+  'commerce',
   'port-de-plaisance',
   'site-industriel',
-  'gare'
+  'gare',
+  // Catégories révisées
+  'bureaux',
+  'depot',
+  'atelier-artisanal',
+  'atelier-industriel',
+  'chenil-vivier',
+  'hotel',
+  'autre-hebergement',
+  'residence-hoteliere',
+  'salle-de-spectacle',
+  'salle-de-loisir',
+  'terrain-de-camping',
+  'etablissement-detente-bien-etre',
+  'centre-de-loisirs',
+  'ecole-privee',
+  'hopital',
+  'centre-medico-social-creche',
+  'maison-de-retraite',
+  'centre-thermal-reeducation',
+  'autre-etablissement'
 ]
 
-function groupByCommune() {
-  const context = {}
+function eachCommune(commune, locaux) {
+  const acceptedLocaux = locaux.filter(l => ACCEPTED_CATEGORIES_LOCAUX.includes(l.categorieLocal))
+  const nbAdressesCadastre = chain(acceptedLocaux).map(l => `${l.codeVoie}-${l.numero}`).uniq().value().length
 
-  return new Transform({
-    transform(row, enc, done) {
-      if (row.codeCommune !== context.currentCommune) {
-        if (context.currentCommune) {
-          this.push({
-            codeCommune: context.currentCommune,
-            locaux: context.locaux
-          })
-        }
+  const communeLocaux = {
+    codeCommune: commune.code,
+    nomCommune: commune.nom,
+    population: commune.population,
+    nbLocaux: acceptedLocaux.length,
+    nbAdressesCadastre
+  }
 
-        context.currentCommune = row.codeCommune
-        context.locaux = []
-      }
-
-      context.locaux.push(row)
-
-      done()
-    },
-
-    flush(done) {
-      this.push({
-        codeCommune: context.currentCommune,
-        locaux: context.locaux
-      })
-
-      done()
-    },
-
-    objectMode: true
-  })
+  return communeLocaux
 }
 
 async function main() {
-  const communesLocaux = await getStream.array(
-    process.stdin
-      .pipe(parse({profile: 'simple'}))
-      .pipe(groupByCommune())
-      .pipe(new Transform({
-        transform({codeCommune, locaux}, enc, done) {
-          const commune = communesIndex[codeCommune]
-
-          if (!commune) {
-            return done()
-          }
-
-          const acceptedLocaux = locaux.filter(l => ACCEPTED_CATEGORIES_LOCAUX.includes(l.categorieLocal))
-          const nbAdressesUniques = chain(acceptedLocaux).map(l => `${l.codeVoie}-${l.numero}`).uniq().value().length
-
-          const communeLocaux = {
-            codeCommune,
-            nomCommune: commune.nom,
-            population: commune.population,
-            nbLocaux: acceptedLocaux.length,
-            nbAdressesUniques
-          }
-
-          if (commune.population) {
-            communeLocaux.adressesRatio = Math.round(nbAdressesUniques / commune.population * 1000)
-          }
-
-          done(null, communeLocaux)
-        },
-        objectMode: true
-      }))
-  )
+  const communesMoselle = communes.filter(c => c.code.startsWith('57'))
+  const communesLocaux = await bluebird.map(communesMoselle, async commune => {
+    const locaux = await getCommuneData(commune.code, {profile: 'simple'})
+    const result = eachCommune(commune, locaux)
+    console.log(`${commune.code} ${commune.nom} OK!`)
+    return result
+  }, {concurrency: 5})
 
   const filePath = join(__dirname, 'dist', 'communes-locaux.json')
-
   await outputJson(filePath, communesLocaux)
 }
 
